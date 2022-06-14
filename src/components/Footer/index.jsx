64b763dc41cca4e5a83fd7554e99626f5f6dcfb2
Ryan Tankeng <92751701+MrRyanWise@users.noreply.github.com>
2022-06-14 13:27:01 +0200

    Add files via upload

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <br />
+          <p>
+            Data Source:&nbsp;
+            <a
+              href="https://github.com/r-spacex/SpaceX-API"
+              rel="noreferrer"
+              target="_blank"
+            >
+              SpaceX API
+            </a>
+          </p>
+          <p>
+            Developed by &copy;
+            <a
+              href="https://github.com/WillN-Git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Will Ndefo
+            </a>
+            &nbsp;&amp;&nbsp;
+            <a
+              href="https://github.com/MrRyanWise"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Ryan Wise 2022
+            </a>
+          </p>
+          <br />
+        </div>
+      </div>
+    </div>
+  );
+}
